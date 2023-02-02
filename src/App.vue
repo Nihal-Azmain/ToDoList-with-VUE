8fb2deccc71c20c072bf96cb66f01adfff2b5449
Nihal-Azmain <n.azmain01@gmail.com>
2023-02-02 09:43:05 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import {ref} from "vue"
+    import inputTask from "./components/inputTask.vue"
+    import tasksLists from "./components/tasksLists.vue"
+    import buttons from "./components/buttons.vue"
+
+    const tasks=[];
+
+</script>
+
+<template>
+    <h1>To-Do APP</h1>
+</template>
