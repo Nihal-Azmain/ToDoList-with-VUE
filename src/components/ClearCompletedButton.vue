1ce9d7b7229b2efe8d2d758d53f2d281b04dace3
Nihal-Azmain <n.azmain01@gmail.com>
2023-02-03 18:44:25 +0600

    final commit
    Signed-off-by: Nihal-Azmain <n.azmain01@gmail.com>

---
+++ src/components/ClearCompletedButton.vue
@@ -0,0 +1,8 @@
+<script setup>
+    defineProps(['isAnySelected']);
+    defineEmits(['clearSelected']);
+</script>
+
+<template>
+    <button @click="$emit('clearSelected')">Clear All Completed tasks</button>
+</template>
